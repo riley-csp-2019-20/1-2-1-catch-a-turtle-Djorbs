643b02167fe6443dac13661f1108fb21e6dc9237
Djorbs <55500961+Djorbs@users.noreply.github.com>
2019-10-24 11:35:26 -0400

    whoops, didn't upload my last versions

---
+++ 121_catch_a_turtle_alexr.py
@@ -0,0 +1,70 @@
+#-----import statements-----
+import turtle as trtl
+import random
+#-----game configuration----
+shape = "turtle"
+size = 2
+color = "brown"
+score = 0
+font_setup = ("Arial", 20, "normal")
+timer = 20
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
+#-----initialize turtle-----
+wack = trtl.Turtle(shape = shape)
+wack.color(color)
+wack.shapesize(size)
+wack.penup()
+wack.speed(0)
+scorer = trtl.Turtle(shape = shape)
+scorer.ht()
+scorer.penup()
+scorer.speed(0)
+scorer.goto(-420, 350)
+counter =  trtl.Turtle()
+counter.ht()
+counter.penup()
+counter.goto(340, 350)
+#-----game functions--------
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    runturtlerun()
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
+def turtlebooped(x,y):
+    change_position()
+    score_count()
+    wack.color(random_color())
+def change_position():
+    wack.ht()
+    new_positx = random.randint(-390, 390)
+    new_posity = random.randint(-390, 390)
+    wack.goto(new_positx, new_posity)
+    wack.st()
+def score_count(): 
+    global score
+    score += 100
+    print(score)
+    font = ("Arial", 30)
+    scorer.clear()
+    scorer.write(score, font = font)
+def runturtlerun():
+    wn.bgcolor(random_color())
+    wack.ht
+    wack.goto(999, 999)
+def random_color():
+    r = random.random()
+    g = random.random()
+    b = random.random()
+    return (r,g,b)
+#-----events----------------
+wack.onclick(turtlebooped)
+wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval) 
+wn.mainloop()
