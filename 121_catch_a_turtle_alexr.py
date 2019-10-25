071740b4ee7ea6635f2f0ccc5d104b796148bfa9
Djorbs <55500961+Djorbs@users.noreply.github.com>
2019-10-25 11:33:38 -0400

    finished

--- 121_catch_a_turtle_alexr.py
+++ 121_catch_a_turtle_alexr.py
@@ -33,36 +33,43 @@ def countdown():
     counter.write("Time's Up", font=font_setup)
     timer_up = True
     runturtlerun()
+    leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
 def turtlebooped(x,y):
-    change_position()
-    score_count()
-    wack.color(random_color())
+  change_position()
+  score_count()
+  wack.color(random_color())
 def change_position():
-    wack.ht()
-    new_positx = random.randint(-390, 390)
-    new_posity = random.randint(-390, 390)
-    wack.goto(new_positx, new_posity)
-    wack.st()
+  wack.ht()
+  new_positx = random.randint(-240, 240)
+  new_posity = random.randint(-240, 240)
+  wack.goto(new_positx, new_posity)
+  wack.st()
 def score_count(): 
-    global score
-    score += 100
-    print(score)
-    font = ("Arial", 30)
-    scorer.clear()
-    scorer.write(score, font = font)
+  global score
+  score += 100
+  font = ("Arial", 30)
+  scorer.clear()
+  scorer.write(score, font = font)
+def leaderboard(): #This is my customization. It brings your score to the middle of the screen
+  global score
+  scorer.clear()
+  scorer.goto(-150,0)
+  font = ("Arial", 30)
+  scorer.write("Your score: " + str(score), font = font)
+
 def runturtlerun():
-    wn.bgcolor(random_color())
-    wack.ht
-    wack.goto(999, 999)
-def random_color():
-    r = random.random()
-    g = random.random()
-    b = random.random()
-    return (r,g,b)
+  wn.bgcolor(random_color())#This is my customization. The background changes to a random color when the game ends.
+  wack.ht
+  wack.goto(999, 999)
+def random_color(): #This is my customization. The turtle changes color everytime it is clicked.
+  r = random.random()
+  g = random.random()
+  b = random.random()
+  return (r,g,b)
 #-----events----------------
 wack.onclick(turtlebooped)
 wn = trtl.Screen()
